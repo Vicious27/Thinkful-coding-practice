9094874c221f9f8ca9c452689b0dd8ac6d11424d
Raul Garcia <punkvomit21@gmail.com>
2021-02-20 16:14:44 -0500

    added tooltip prop and styled according to classNames

--- src/composition/Tooltip.js
+++ src/composition/Tooltip.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import './Tooltip.css';
 
 function Tooltip(props) {
   return (
     <span className="Tooltip">
-
+      <span className="Tooltip-content">
+        {props.children}
+      </span>
+      <div classname="Tooltip-message">
+        {props.message}
+      </div>
     </span>
   );
 }
